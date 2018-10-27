ceaec6dff331264237dacf9561fbf1e6b7328226
EC2 Server <i@TianyuDu.com>
2018-10-27 22:12:18 +0000

    update constants

--- constants.py
+++ constants.py
@@ -3,8 +3,8 @@ CONSTANTS
 """
 
 SAVED_FILE_DIRS = {
-    "MAC": "/Users/tianyudu/Documents/Activities/UTCAA-Mentorship-2018/data/application_train.csv"
-    "DRIVE": "/Volumes/Intel/Data/UTCAA-Mentorship-2018/application_train.csv"
-    "EC2": "/mentor_2018/data/application_train.csv"
+    "MAC": "/Users/tianyudu/Documents/Activities/UTCAA-Mentorship-2018/data/application_train.csv",
+    "DRIVE": "/Volumes/Intel/Data/UTCAA-Mentorship-2018/application_train.csv",
+    "EC2": "/mentor_2018/data/application_train.csv",
     "CLOUD": None
 }
