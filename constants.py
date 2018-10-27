bfcd968e41a78edf9649d0ff86e6ef42e4b58254
EC2 Server <i@TianyuDu.com>
2018-10-27 22:17:22 +0000

    Update constants

--- constants.py
+++ constants.py
@@ -5,6 +5,6 @@ CONSTANTS
 SAVED_FILE_DIRS = {
     "MAC": "/Users/tianyudu/Documents/Activities/UTCAA-Mentorship-2018/data/application_train.csv",
     "DRIVE": "/Volumes/Intel/Data/UTCAA-Mentorship-2018/application_train.csv",
-    "EC2": "/mentor_2018/data/application_train.csv",
+    "EC2": "/home/ec2-user/environment/mentor_2018/data/application_train.csv",
     "CLOUD": None
 }
--- ui_control.py
+++ ui_control.py
@@ -1,15 +1,18 @@
 """
 UI control tools
 """
+from pprint import pprint
+from typing import Dict
 
-def choose_dataset(FILE_DIRS: dict) -> str:
+
+def choose_dataset(file_dirs: Dict[str, str]) -> str:
     print("Data set avaiable: ")
-    pprint(FILE_DIRS)
+    pprint(file_dirs)
     dataset_selection = input("Where is the dataset? [C: customize]>>> ")
     if dataset_selection.lower() == "c":
         chosen = input("Dataset dir >>> ")
     try:
-        chosen = FILE_DIRS[dataset_selection.upper()]
+        chosen = file_dirs[dataset_selection.upper()]
     except KeyError:
         raise Warning("Dataset not found.")
     return  chosen
