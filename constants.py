77199787c9cf380dda267c6e3537723152e4f446
EC2 Default User <ec2-user@ip-172-31-89-216.ec2.internal>
2018-11-02 04:51:58 +0000

    update

--- constants.py
+++ constants.py
@@ -6,7 +6,8 @@ SAVED_FILE_DIRS = {
     "MAC": "/Users/tianyudu/Documents/Activities/UTCAA-Mentorship-2018/data/application_train.csv",
     "DRIVE": "/Volumes/Intel/Data/UTCAA-Mentorship-2018/application_train.csv",
     "EC2": "/home/ec2-user/environment/mentor_2018/data/application_train.csv",
-    "CLOUD": None
+    "CLOUD": "https://s3.us-east-2.amazonaws.com/tianyudu/kaggle/application_train.csv",
+    "EC2_server": "/home/ec2-user/data/application_train.csv"
 }
 
 PATH_EXTEND = [
